db344ecd16489ff6fa523451488cf0ebbe80bfe1
Ashiq121914 <ashiq121914@gmail.com>
2024-12-16 03:58:02 +0600

    add login endpoint

--- backend/src/user/userController.ts
+++ backend/src/user/userController.ts
@@ -59,3 +59,39 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(createHttpError(500, "Error while signing jwt token"));
   }
 };
+
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  const user = await userModel.findOne({ email });
+  try {
+    if (!user) {
+      return next(createHttpError(404, "user not found"));
+    }
+  } catch (err) {
+    return next(createHttpError(500, "Error while getting user"));
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return next(createHttpError(400, "Username or password incorrect!"));
+  }
+
+  try {
+    // token generation
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+
+    res.json({ accessToken: token });
+  } catch (err) {
+    return next(createHttpError(500, "Error while signing jwt token"));
+  }
+};
+export { createUser, loginUser };
