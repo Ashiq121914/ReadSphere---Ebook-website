c16f4eadf5e8577599afdd7569fcf2ef29d680f3
Ashiq121914 <ashiq121914@gmail.com>
2024-12-16 21:26:41 +0600

    create book

--- backend/src/book/bookController.ts
+++ backend/src/book/bookController.ts
@@ -2,8 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
+import fs from "node:fs";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  const { title, genre } = req.body;
   try {
     // cloudinary setup for image
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
@@ -40,7 +43,22 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
       }
     );
 
-    res.json({});
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "675f3db93aa23850a8391822",
+      coverImage: uploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+
+    try {
+      // delete temp files
+      await fs.promises.unlink(filePath);
+      await fs.promises.unlink(bookFilePath);
+    } catch (err) {
+      return next(createHttpError(500, "error while deleting the temp files"));
+    }
+    res.status(201).json({ id: newBook._id });
   } catch (error) {
     console.log(error);
     return next(createHttpError(500, "Error while uploading the files"));
