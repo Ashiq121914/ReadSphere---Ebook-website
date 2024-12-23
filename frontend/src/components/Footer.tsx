5d5edbc61ae4568d02d4dd3f9cf2c6f9710deda5
Ashiq121914 <ashiq121914@gmail.com>
2024-12-23 09:21:00 +0600

    add details book and footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="border-t p-5 text-center absolute bottom-0 w-full ">
+      <span>© 2024 ReadSphere, All rights reserved.</span>
+    </footer>
+  );
+};
+
+export default Footer;
