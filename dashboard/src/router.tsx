e6e9e471518aacb551f2bb8e3c77f3f80e83efd1
Ashiq121914 <ashiq121914@gmail.com>
2024-12-30 04:55:08 +0600

    add dashboard layout

--- dashboard/src/router.tsx
+++ dashboard/src/router.tsx
@@ -2,11 +2,23 @@ import { createBrowserRouter } from "react-router";
 import LoginPage from "./pages/Login";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegsiterPage";
+import DashboardLayout from "./layouts/DashboardLayout";
+import BooksPage from "./pages/BooksPage";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
+    path: "dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "books",
+        element: <BooksPage />,
+      },
+    ],
   },
   {
     path: "/login",
